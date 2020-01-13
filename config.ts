3af4a37c92dee99022ae37258c7e8956306b8e08
Kamen Minkov <kamen.minkov@gmail.com>
2020-01-13 21:48:49 +0200

    Change default config

--- config.ts
+++ config.ts
@@ -1,3 +1,3 @@
-export const WRITE_CLEAN_BOOKMARKS_IN_PLACE: boolean = true; // whether to write cleaned up bookmarks back in their original location - useful when multiple paths are provided
+export const WRITE_CLEAN_BOOKMARKS_IN_PLACE: boolean = false; // whether to write cleaned up bookmarks back in their original location - useful when multiple paths are provided
 
 export const DEFAULT_INPUT_FILENAME: string = 'Bookmarks';
